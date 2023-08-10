8af0d9799d270263717aa53d8a71f9004cab86e6
NareshKumar K <53271957+Nareshk170999@users.noreply.github.com>
2023-08-10 22:17:25 +0530

    Update Main.java
    submit4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,8 +15,9 @@ class B extends A {
 
 public class Main {
   public static void main(String[] args) {
-    A objA = new B();
-//    System.out.println(objA.meth());
-    System.out.println(objA.meth());
+    B objB = new B();
+
+    String resultFromA = objB.meth();
+    System.out.println(resultFromA);
   }
 }
