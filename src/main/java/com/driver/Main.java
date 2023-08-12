7bfbafd4b2f13b4a027d756303d5a95a4bfda764
NareshKumar K <53271957+Nareshk170999@users.noreply.github.com>
2023-08-12 20:32:44 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,15 +9,15 @@ class A {
 class B extends A {
   @Override
   String meth() {
+
     return "Method is overridden in Extended class B";
   }
 }
 
 public class Main {
   public static void main(String[] args) {
-    B objB = new B();
-
-    String resultFromA = objB.meth();
-    System.out.println(resultFromA);
+    B b = new B();
+    System.out.println(b.meth());
   }
 }
+
